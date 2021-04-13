6db2dd9444715664e6c21c6a950b7bf90cd05cce
EricksonDutra <ericksond10@gmail.com>
2021-04-13 12:39:14 -0400

    Criado atributo complexo: filhos.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,17 +1,21 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=30):
+    def __init__(self, *filhos, nome=None, idade=30):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa('Dutra')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Erickson'
-    print(p.nome)
+    erickson = Pessoa(nome='Erickson')
+    ramao = Pessoa(erickson, nome='Ramão')
+    print(Pessoa.cumprimentar(erickson))
+    print(id(erickson))
+    print(erickson.cumprimentar())
+    print(erickson.nome)
+    print(erickson.idade)
+    for filho in ramao.filhos:
+        print('Os filhos de ' + ramao.nome + ' são: ' + filho.nome)
+
