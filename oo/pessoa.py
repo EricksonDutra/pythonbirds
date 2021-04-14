cbb5d39ec7bbe8d0d009f6aad36f4230ba9811e3
EricksonDutra <ericksond10@gmail.com>
2021-04-14 12:40:05 -0400

    Criado metodo de classe.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -9,6 +9,14 @@ class Pessoa:
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
+    @staticmethod
+    def metodo_estatico():
+        return 42
+
+    @classmethod
+    def nome_e_atributos_de_classe(cls):
+        return f'{cls} - olhos {cls.olhos}'
+
 
 if __name__ == '__main__':
     erickson = Pessoa(nome='Erickson')
@@ -30,4 +38,7 @@ if __name__ == '__main__':
     print(erickson.olhos)
     print(ramao.olhos)
     print(id(Pessoa.olhos), id(erickson.olhos), id(ramao.olhos))
+    print(Pessoa.metodo_estatico())
+    print(Pessoa.nome_e_atributos_de_classe(), erickson.nome_e_atributos_de_classe())
+
 
